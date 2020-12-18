4d57e379260c752c1323ec0309dac9cc51baba17
Anh Quang <anhquang2605@gmail.com>
2020-12-17 16:37:46 -0800

    Added a bunch of files added a bunch of file

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends Components {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <h1>Commercial website</h1>
+            </div>
+        )
+    }
+} 
