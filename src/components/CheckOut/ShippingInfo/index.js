800055099befe0905076ed97a65cf7457572fc74
Anh Quang <anhquang2605@gmail.com>
2021-02-02 17:56:09 -0800

    Added shipping list, created shipping collection on fire store, added basic form backbone for adding new shipping information as well as  displaying option as radioa button for user to choose desired address, created collapsable plugin

--- src/components/CheckOut/ShippingInfo/index.js
+++ src/components/CheckOut/ShippingInfo/index.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './shipping-info.scss';
+import Collapsable from "../../Plugins/Collaspable";
+import firebase from '../../../firebase';
 const shippingInitObj = {
     name: "Pikachu",
     address: "3637 Snell Ave",
@@ -8,25 +10,108 @@ const shippingInitObj = {
     city: "San Jose",
     zip: "95236"
 }
-
+const db = firebase.firestore();
 const ShippingInfo = () => {
-    const [shippingList, setcurrentShipping] = useState([]);
+    //States
+    const [shippingList, setShippingList] = useState([]);
     const [currentShipping, setCurrentShipping] = useState(shippingInitObj)
-    var allStates;
-    fetch("USstates.json").then((dat)=>dat.json()).then(data=>{
-        allStates = data;
-    });
+    const [allStates, setAllStates] = useState();
+    const [shippingFormValues, setShippingFormValues] = useState({});
+    //Methods
+
+    //Getting states data from json file at public directory
+    useEffect(() => {
+        db.collection("shippings").get().then((dat)=>{
+            var collections = dat.docs;
+            var list = [];
+            var promise = new Promise((resolve)=>{
+                collections.forEach((doc)=>{
+                    list.push(doc.data())
+                });
+                resolve();
+             });
+            promise.then(()=>{
+                setShippingList(list);
+            });
+        })
+        fetch("USstates.json").then((dat)=>dat.json()).then(data=>{
+            setAllStates(data);
+        });
+    }, []);
     return (
         <div id="shipping-info">
                 <h4 className="check-out-title">Shipping</h4>
-                {currentShipping && 
-                    <div className="shipping-chosen">
-                        <button className="change-btn">Change</button>
-                        <span className="chosen-shipping-name">{currentShipping.name}</span>
-                        <span className="chosen-shipping-address-line">{currentShipping.address} #{currentShipping.apt}</span>
-                        <span className="chosen-shipping-city-state">{currentShipping.city}, {currentShipping.resiState} {currentShipping.zip} </span>
-                    </div>
-                }
+                <Collapsable
+                    itemName="shipping"
+                   //Top part of the collapsable plugin
+                    chosenChildren= {currentShipping && 
+                        <div className="shipping-chosen">
+                            <span className="chosen-shipping-name">{currentShipping.name}</span>
+                            <span className="chosen-shipping-address-line">{currentShipping.address} #{currentShipping.apt}</span>
+                            <span className="chosen-shipping-city-state">{currentShipping.city}, {currentShipping.resiState} {currentShipping.zip} </span>
+                        </div>
+                    }
+                    //Bottom part of the collapsable plugin where change field and form present
+                    changeChildren={
+                        (
+                        <div>
+                            <div className="shipping-options">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>Name</th>
+                                            <th>Address</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {shippingList && shippingList.map((ship)=>{
+                                        return(
+                                       <tr key={ship.id}>
+                                           <td><input type="radio"></input></td>
+                                           <td className="ship-name">{ship.name}</td>
+                                           <td className="ship-address">{ship.address}</td>
+                                       </tr>     
+                                        );
+                                    })}
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="shipping-adding">
+                                <span className="add-shipping-name">
+                                    <label className="label">For (Name):</label>
+                                    <input value={shippingFormValues.name} type="text" placeholder="Enter Name Here"></input>
+                                </span>
+                                <span className="add-shipping-address">
+                                    <label className="label">Address</label>
+                                    <input value={shippingFormValues.address} type="text" placeholder="Enter Address"></input>
+                                </span>
+                                <span className="add-shipping-state">
+                                    <label className="label">State</label>
+                                    <select value={shippingFormValues.resiState}>
+                                        {allStates && allStates.map((daState)=>{
+                                            return(
+                                                <option value={daState.abbreviation}>{daState.name}</option>
+                                            );
+                                        })}
+                                    </select>
+                                </span>
+                                <span className="add-shipping-city">
+                                    <label className="label">City</label>
+                                    <input value={shippingFormValues.city} type="text" placeholder="Enter City"></input>
+                                </span>
+                                <span className="add-shipping-zip">
+                                    <label className="label">Zip</label>
+                                    <input value={shippingFormValues.zip} type="text" placeholder="Enter zip code"></input>
+                                </span>
+                            </div>
+                        </div>
+                        )
+                    }
+                >
+
+                </Collapsable>
+               
                 <div className="Shipping-changed">
 
                 </div>
---
+++ src/components/Plugins/Collaspable/index.js
@@ -0,0 +1,43 @@
+import React, {useState,useEffect, useImperativeHandle} from 'react';
+import './collapsable.scss';
+const Collapsable = React.forwardRef((props,ref) => {
+    //States
+    let [idChosenDiv, setIdChosenDiv] = useState("chosen_"+props.itemName);
+    let [idChangeDiv, setIdChangeDiv] = useState("change_field_"+props.itemName);
+
+
+    //Methods of the component
+      //When user click change button to change their cards option
+    let handleChange = (e) =>{
+        e.stopPropagation();
+        var changeField = document.getElementById(idChangeDiv);
+        var chosen = document.getElementById(idChosenDiv);
+        changeField.classList.remove("display-none");
+        chosen.classList.add("display-none");
+    }
+      //when use click close button or commit to change to hide the change option tab
+    let handleClose = (e) =>{
+       e.stopPropagation();
+       var changeField = document.getElementById(idChangeDiv);
+       var chosen = document.getElementById(idChosenDiv);
+        chosen.classList.remove("display-none");
+        changeField.classList.add("display-none");   
+    }
+    useImperativeHandle(ref, ()=>({//expose the component methods to its user, must use useRef to refer to it 
+
+    }));
+    return (
+        <div className="collapsable">
+            <div id={idChosenDiv} className="chosen-collapsable">
+                {props.chosenChildren}
+                <button className="change-btn" onClick={handleChange}>Change</button>
+            </div>
+            <div id={idChangeDiv} className="change-collapsable display-none">
+                {props.changeChildren}
+                <button className="close-btn" onClick={handleClose}>Commit Change (Close)</button>
+            </div>
+        </div>
+    );
+});
+
+export default Collapsable;
